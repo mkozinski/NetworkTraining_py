e2c94ca6b404dd278d5be2e0fc0664a1e2fcfac1
Mateusz Kozinski <kozinski@iccluster132.iccluster.epfl.ch>
2019-06-04 01:40:44 +0200

    modified:   datasetCrops.py 	handles the case where the input has one more channel than the target

--- datasetCrops.py
+++ datasetCrops.py
@@ -9,7 +9,8 @@ class TestDataset(Dataset):
 # by cutting it into overlapping crops/tiles
 # but only retaining ground truths for the non-overlapping regions
 
-  def __init__(self, img, lbl, cropSz, margSz, augment, ignoreInd=255):
+  def __init__(self, img, lbl, cropSz, margSz, augment, ignoreInd=255, 
+               imgHasChannelDim=False):
     self.cropSz=cropSz
     self.margSz=margSz
     self.img=img
@@ -17,6 +18,7 @@ class TestDataset(Dataset):
     self.no_crops=[0] # no_crops[i] = sum no crops in img[k] for all k<i
     self.ignoreInd=ignoreInd
     self.augment=augment
+    self.imgHasChannelDim=imgHasChannelDim
     for l in self.lbl:
       tot_no_crops=cropRoutines.noCrops(l.shape,self.cropSz,self.margSz,0)+\
                    self.no_crops[-1]
@@ -32,7 +34,10 @@ class TestDataset(Dataset):
     img=self.img[ind]
     cropInd=idx-self.no_crops[ind] # index of crop of img[ind]
     cc,vc,_=cropRoutines.cropCoords(cropInd,self.cropSz,self.margSz,lbl.shape,0)
-    cimg=img[tuple(cc)].copy()
+    if self.imgHasChannelDim:
+      cimg=img[tuple([slice(0,img.shape[0])]+cc)].copy()
+    else:
+      cimg=img[tuple(cc)].copy()
     clbl=lbl[tuple(cc)].copy()
     # use vc to inpaint margins to ignore in lbl!
     idx=[]
