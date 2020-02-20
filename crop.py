45e7cb115bc6f4540c8e1ed5a917254ed704e756
Mateusz Kozinski <kozinski@iccluster133.iccluster.epfl.ch>
2020-02-20 17:15:17 +0100

    modified:   crop.py	it suffices to specify last k indices of the crop, so tensors with different number of trailing dimensions can be cropped the same way

--- crop.py
+++ crop.py
@@ -3,22 +3,36 @@ import numpy as np
 def crop(img,inds,fill=0):
     # crop where the range of indexes can contain negative values
     # that is, we can request a crop that goes outside of the image frame
-    # this means, negatice indeces to the left (top) of the image
-    # and indeces larger than the image size to the right (bottom)
-    # inds is a list of slice objects
+    # img is the numpy array to be cropped
+    # inds is a list of slice objects defining the crop
+    # negative values denote positions to the left (top) of the image
+    # and values larger than image size denote positions to the right (bot)
+    # when inds is shorter than img.shape, the initial k missing dimensions
+    # are taken to be img.shape[n] where 0<=n<k
     # fill is the value inpainted in the regions outside of the image frame
     outsize=[]
     dstinds=[]
     srcinds=[]
     for k in range(len(inds)):
-        srcbeg=max(0,inds[k].start)
-        srcend=min(img.shape[k],inds[k].stop)
-        dstbeg=max(0,-inds[k].start)
+        i=k+1 # index from the back
+        ind=inds[-i]
+        shp=img.shape[-i]
+        srcbeg=max(0,  ind.start)
+        srcend=min(shp,ind.stop)
+        dstbeg=max(0, -ind.start)
         dstend=dstbeg+srcend-srcbeg
-        dstinds.append(slice(dstbeg,dstend))
-        srcinds.append(slice(srcbeg,srcend))
-        outsize.append(inds[k].stop-inds[k].start)
+        dstinds.insert(0,slice(dstbeg,dstend))
+        srcinds.insert(0,slice(srcbeg,srcend))
+        outsize.insert(0,ind.stop-ind.start)
+
+    # default values for the initial dimensions
+    missing_dims=img.ndim-len(inds)
+    dstinds=[slice(None)]*missing_dims     +dstinds
+    srcinds=[slice(None)]*missing_dims     +srcinds
+    outsize=list(img.shape)[0:missing_dims]+outsize
+
     crop=np.full(tuple(outsize),fill,img.dtype)
+    print(dstinds,srcinds)
     crop[tuple(dstinds)]=img[tuple(srcinds)]
     return crop, dstinds
 
