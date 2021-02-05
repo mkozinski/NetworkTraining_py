01a8a3daf8b8b9ca7d0728edb2a66cb3115c2b84
Mateusz Kozinski <kozinski@iccvlabsrv27.iccluster.epfl.ch>
2021-02-05 20:35:15 +0100

    modified:   loggerBasic.py interface to the "add" method changed

--- loggerBasic.py
+++ loggerBasic.py
@@ -2,7 +2,7 @@ import os
 import torch
 
 class LoggerBasic:
-    def __init__(self, log_dir, name, saveNetEvery=500):
+    def __init__(self, log_dir, name, saveNetEvery=500, saveAndKeep=False):
         self.log_dir=log_dir
         self.log_file=os.path.join(self.log_dir,"log"+name+".txt")
 
@@ -11,13 +11,14 @@ class LoggerBasic:
         self.loss=0
         self.count=0
         self.saveNetEvery=saveNetEvery
+        self.saveAndKeep=saveAndKeep
         self.epoch=0
 
-    def add(self,l,output,target):
+    def add(self,img,output,target,l,net=None,optim=None):
         self.loss+=l
         self.count+=1
 
-    def logEpoch(self,net):
+    def logEpoch(self,net=None,optim=None,scheduler=None):
         text_file = open(self.log_file, "a")
         text_file.write(str(self.loss/self.count))
         text_file.write('\n')
@@ -27,8 +28,20 @@ class LoggerBasic:
         self.count=0
         self.epoch+=1
         if self.epoch % self.saveNetEvery == 0:
-          torch.save({'epoch': self.epoch,
-                      'state_dict': net.state_dict()},
-                       os.path.join(self.log_dir, 
-                       'net_last.pth'))
+          if self.saveAndKeep:
+            fname='epoch_'+str(self.epoch)+'.pth'
+          else:
+            fname='last.pth'
+          if net:
+            nfname='net_'+fname
+            torch.save({'epoch': self.epoch, 'state_dict': net.state_dict()},
+                       os.path.join(self.log_dir,nfname))
+          if optim:
+            ofname='optim_'+fname
+            torch.save({'epoch': self.epoch, 'state_dict': optim.state_dict()},
+                       os.path.join(self.log_dir,ofname))
+          if scheduler:
+            ofname='scheduler_'+fname
+            torch.save({'epoch': self.epoch, 'state_dict': scheduler.state_dict()},
+                       os.path.join(self.log_dir,ofname))
         return lastLoss
